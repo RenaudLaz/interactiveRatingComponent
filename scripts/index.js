2a532561c3645c8a348f7f9c439f8a14fe447122
Renaud Lazardeux <Renaud.lazardeux@gmail.com>
2022-05-25 22:22:16 +0200

    version desktop

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,57 @@
-const button = document.querySelector('button');
-button.addEventListener('click', event => {
-  button.classList.add('buttonChoice2')
-  button.classList.remove('buttonChoice')
+const buttons = document.querySelectorAll(".buttonNumbers button");
+const submit = document.getElementById("submit");
+const error = document.getElementById("error-msg");
 
-});
+const selectionMsg = document.getElementById("selection-msg");
+const box1 = document.getElementById("ratingState");
+const box2 = document.getElementById("thankYou");
+let selected = "";
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        removeSelectedClasses()
+        selected = e.target.textContent
+        e.target.classList.add("selected")
+    })
+})
+
+submit.addEventListener("click", (e) => {
+    if (selected === "") {
+        console.log(submit)
+
+        addButtonErrors()
+        setTimeout(() => {
+            removeButtonErrors()
+        }, 3000);
+        return
+    }
+
+    selectionMsg.textContent = `You selected ${selected} out of 5`
+    ratingState.classList.add("d-none");
+    thankYou.classList.remove("d-none");
+    console.log(thankYou)    
+    console.log(ratingState)
+})
+
+function addButtonErrors()
+{
+    error.classList.remove("d-none")
+    buttons.forEach(btn => {
+        btn.classList.add("error")
+    })
+}
+
+function removeButtonErrors()
+{
+    error.classList.add("d-none")
+    buttons.forEach(btn => {
+        btn.classList.remove("error")
+    })
+}
+
+function removeSelectedClasses()
+{
+    buttons.forEach(btn => {
+        btn.classList.remove("selected")
+    })
+}
